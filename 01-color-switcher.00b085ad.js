890e06275ed1cd166bab2a5db9b63e6074380e16
JKoprowicz2000 <143550412+JKoprowicz2000@users.noreply.github.com>
2024-03-06 19:10:25 +0000

    Deploying to gh-pages from @ JKoprowicz2000/goit-js-hw-09@60059e5b09af1acc1310a71d540ce9bcf55a8315 🚀

---
+++ 01-color-switcher.00b085ad.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector("[data-start]"),o=document.querySelector("[data-stop]");function d(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16)),t=setTimeout(d,1e3)}e.addEventListener("click",(function(){e.disabled=!0,o.disabled=!1,d()})),o.addEventListener("click",(function(){e.disabled=!1,o.disabled=!0,clearTimeout(t),document.body.style.backgroundColor="#ffffff"}))}();
+//# sourceMappingURL=01-color-switcher.00b085ad.js.map

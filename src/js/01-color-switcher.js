2e9c27b3a3a96df2a6d156abbdab40e2f80d9b1b
JKoprowicz2000 <143550412+JKoprowicz2000@users.noreply.github.com>
2024-03-06 20:08:26 +0100

    123

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]')
+
+let timeoutId;
+
+
+function getRandomHexColor() {  
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    }
+    function changeBackgroundColor() {
+        document.body.style.backgroundColor = getRandomHexColor()
+       timeoutId = setTimeout(changeBackgroundColor, 1000)
+    }
+
+    startButton.addEventListener('click', () => {
+        startButton.disabled = true;
+        // Włącz przycisk Stop
+        stopButton.disabled = false;
+        // Rozpocznij zmianę koloru tła
+        changeBackgroundColor();
+    });
+        stopButton.addEventListener('click', () => {
+            // Włącz przycisk Start
+            startButton.disabled = false;
+            // Wyłącz przycisk Stop
+            stopButton.disabled = true;
+            clearTimeout(timeoutId);
+        });
 

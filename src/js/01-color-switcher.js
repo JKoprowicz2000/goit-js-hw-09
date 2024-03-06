6f63c9314af43990af061acd66118bcf20a9db36
JKoprowicz2000 <143550412+JKoprowicz2000@users.noreply.github.com>
2024-03-06 20:54:31 +0100

    12345

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,30 +1,18 @@
-const startButton = document.querySelector('[data-start]');
-const stopButton = document.querySelector('[data-stop]')
-
-let timeoutId;
-
-
-function getRandomHexColor() {  
+function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-    }
-    function changeBackgroundColor() {
-        document.body.style.backgroundColor = getRandomHexColor()
-       timeoutId = setTimeout(changeBackgroundColor, 1000)
-    }
+    }    
+    let intervalId;
+function changeBackgroundColor() {
+     document.body.style.backgroundColor = getRandomHexColor();
+}
+document.querySelector('[data-start]').addEventListener('click',function() {
+   this.disabled = true;
+   document.querySelector('[data-stop]').disabled = false;
+   intervalId = setInterval(changeBackgroundColor, 1000);
+});
+document.querySelector('[data-stop]').addEventListener('click',function() {
+this.disabled = true;
 
-    startButton.addEventListener('click', () => {
-        startButton.disabled = true;
-        // Włącz przycisk Stop
-        stopButton.disabled = false;
-        // Rozpocznij zmianę koloru tła
-        changeBackgroundColor();
-    });
-        stopButton.addEventListener('click', () => {
-            // Włącz przycisk Start
-            startButton.disabled = false;
-            // Wyłącz przycisk Stop
-            stopButton.disabled = true;
-            clearTimeout(timeoutId);
-            document.body.style.backgroundColor = '#ffffff';
-        });
-
+document.querySelector('[data-start]').disabled = false;
+clearInterval(intervalId);
+});
